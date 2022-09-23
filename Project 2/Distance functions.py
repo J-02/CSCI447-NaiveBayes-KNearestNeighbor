6d07e8381912b828e7524e74895e8e99ecb040d1
jonhenk <Jon.henk3@gmail.com>
2022-09-23 14:10:47 -0600

    Added python files to project 2

--- Project 2/Distance functions.py
+++ Project 2/Distance functions.py
@@ -1,8 +1,8 @@
 # Need a Discrete and Continuous distance function
 
 
-#
-#
+# Value difference metric
+# takes two data points and finds the differences between each discrete feature
 #
 #
 def VDM(X1, X2):
