d9a45318d098fa2aef8c43e8ee5f588ab3d9d2a9
jon <jonathanhenke@icloud.com>
2022-09-26 20:12:49 -0600

    Modifed data files and added sampling function in CrossValidation.py

--- Project 2/CrossValidation.py
+++ Project 2/CrossValidation.py
@@ -1,1 +1,20 @@
+import numpy as np
+import pandas as pd
+import os
+
+# getSamples
+# --------------------------------------
 # Splits dataset into 10 stratified samples
+# input is a .data file in the /Data/ folder
+# returns 10 stratified dataframes
+def getSamples(dataset):
+    df = pd.read_csv("Data/"+dataset,index_col=0)
+    proportions = []
+    samples = []
+    for i in range(10):
+        n = 1/(10-i)
+        sample = df.groupby('class', group_keys=False).apply(lambda x: x.sample(frac=n, replace=False, random_state=0))
+        df = df.drop(sample.index)
+        samples.append(sample)
+    return samples
+
---
+++ Project 2/EK-NN.py
@@ -0,0 +1,7 @@
+# Edited Nearest Neighbor
+# -----------------------
+# Classifies points in data set until the performance stops improving
+
+
+def EK_NN(data, k, e):
+    pass
