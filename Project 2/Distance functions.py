4cd57843a02e06ea32322b6169d4a6a0edc20928
jon <jonathanhenke@icloud.com>
2022-09-27 21:26:58 -0600

    Finished VDM and FDM

--- Project 2/Distance functions.py
+++ Project 2/Distance functions.py
@@ -2,11 +2,32 @@ import pandas as pd
 import numpy as np
 import CrossValidation as cv
 
-
+# Value difference metric
+#---------------------------------------------------------------------
+# takes two data points and finds the differences between each discrete feature returns aggregate distance between features
+# if feature not on feature difference matrix defaults to 0 for distance
+def VDM(data, X, Y, p=2):
+    FDMs = FDM(data)
+    d = 0
+    for i in range(len(FDMs)):
+        matrix = FDMs[i]
+        matrix.columns = matrix.columns.astype(str)
+        matrix.index = matrix.index.astype(str)
+        x = str(X.iloc[0][i])
+        y = str(Y.iloc[0][i])
+        #print(matrix)
+        if x in matrix.columns and y in matrix.columns:
+            dist = matrix.loc[x,y]
+        else:
+            dist = 0
+        d += np.power(dist,(1/p))
+    return d
 
 # FeatureDifferenceMatrix: input is dataframe
-# given data constructs matrix of differences between values
-# has issues with building matricies properly, will try to use data frames
+# given data constructs a matrix for each feature of differences between values
+# returns list of dataframes corresponding to index of feature
+# p is set to 2 by default and can be changed
+# used to calculate VDM
 def FDM(data,p=2):
 
     FDMs = []
@@ -15,7 +36,7 @@ def FDM(data,p=2):
     columns = data.columns[data.columns != 'class']
     for col in columns:
         if data[col].dtype != "float64":
-            matrix = np.array([])
+            matrix = pd.DataFrame()
             for Class in classes:
                 pvalues = []
                 for value in data[col].unique():
@@ -25,40 +46,27 @@ def FDM(data,p=2):
                     pvalues.append(P)
                 pvalues = np.power(np.absolute((np.subtract.outer(pvalues, pvalues))),p)
                 if matrix.size == 0:
-                    matrix = pvalues
+                    matrix = pd.DataFrame(data=pvalues, index=data[col].unique(), columns=data[col].unique())
                 else:
                     matrix = np.add(pvalues,matrix)
             FDMs.append(matrix)
     return FDMs
 
-# Value difference metric
-# takes two data points and finds the differences between each discrete feature
-def VDM(data, X, Y, p=2):
-    FDMs = FDM(data)
-    d = 0
-    for i in range(len(FDMs)):
-        matrix = FDMs[i]
-        x = X.iloc[0][i]-1
-        y = Y.iloc[0][i]-1
-        if matrix.shape[0] < x or matrix.shape[1] < y:
-            dist = 10000
-        else:
-            dist = matrix[x][y]
-        d += np.power(dist,(1/p))
-    return d
+#-----------------------------------------------------------
 
 
 
 data = cv.getSamples('soybean-small.data')
 train = pd.DataFrame()
-for i in range(data.__sizeof__()):
+for i in range(len(data)-1):
     if train.size == 0:
         train = data[i]
     else:
         train = pd.merge(train,data[i])
 
-    x = data[i+1].sample(n=1)
-    print(x)
-    y = data[i+1].sample(n=1,random_state=1100)
-    print(y)
+    samples = data[i+1].sample(n=2)
+    x = samples.iloc[[0]]
+    y = samples.drop(x.index)
+    #print(x)
+    #print(y)
     print(VDM(train,x,y))
