5c4bc13532b120c8fd0d032099241e29eb5d3abf
jon <jonathanhenke@icloud.com>
2022-10-03 10:43:42 -0600

    ADDED REGRESSION SAMPLING AND EDITED COLUMNS OF REGRESSION DATA

--- Project 2/CrossValidation.py
+++ Project 2/CrossValidation.py
@@ -9,16 +9,32 @@ import os
 # returns 10 stratified dataframes
 def getSamples(dataset):
     df = pd.read_csv("Data/"+dataset,index_col=0)
-    print(df.shape)
 
     samples = []
-    for i in range(10):
-        n = 1/(10-i)
-        sample = df.groupby('class', group_keys=False).apply(lambda x: x.sample(frac=n, replace=False, random_state=0))
 
-        df = df.drop(sample.index)
-        samples.append(sample)
+# Creates samples for classification
 
-    return samples
+    if 'class' in df.columns:
+        for i in range(10):
+            n = 1/(10-i)
+            sample = df.groupby('class', group_keys=False).apply(lambda x: x.sample(frac=n, replace=False, random_state=0))
+            df = df.drop(sample.index)
+            samples.append(sample)
 
-getSamples('abalone.data')
+        return samples
+
+# Creates samples for regression
+
+    else:
+        df = df.sort_values(by=df.columns[-1])
+        for i in range(10):
+            sample = df.iloc[i::10, :]
+            samples.append(sample)
+
+        return samples
+
+def CrossValidation():
+
+
+
+print(getSamples('abalone.data'))
