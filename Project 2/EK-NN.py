9838af96767c5696817c1679b751e1923bf1f91a
jon <jonathanhenke@icloud.com>
2022-10-03 10:45:05 -0600

    began creating nearest neighbor class

--- Project 2/EK-NN.py
+++ Project 2/EK-NN.py
@@ -3,14 +3,16 @@ import numpy as np
 import CrossValidation as cv
 
 
-# Edited Nearest Neighbor
+# Nearest Neighbor
 # -----------------------
-# Classifies points in data set until the performance stops improving
-class NearestNeightbor:
+# Will add all nearest neighbor algorithms here
+class NearestNeighbor:
 
     def __init__(self, data):
         self.samples = cv.getSamples(data)
 
+
+# Classifies points in data set until the performance stops improving
     def EKNN(k,n):
         pass
 
