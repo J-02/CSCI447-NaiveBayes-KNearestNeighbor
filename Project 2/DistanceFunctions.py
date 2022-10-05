d846a7b389a05a12102ee2794aa7ef4d0b3c814d
jonhenk <Jon.henk3@gmail.com>
2022-10-05 16:46:11 -0600

    update

--- Project 2/DistanceFunctions.py
+++ Project 2/DistanceFunctions.py
@@ -7,7 +7,7 @@ import CrossValidation as cv
 # takes two data points and finds the differences between each discrete feature returns aggregate distance between features
 # if feature not on feature difference matrix defaults to 0 for distance
 def VDM(data, X, Y, p=2):
-    FDMs = FDM(data)
+    FDMs = FDM(data) #
     d = 0
     for i in range(len(FDMs)):
         matrix = FDMs[i]
@@ -58,6 +58,35 @@ def FDM(data,p=2):
     return FDMs
 #-----------------------------------------------------------
 
+# new vdm:
+def initialize(data):
+    pass
+def VDM2(data, x, y):
+    x = x.to_dict('records')[0]
+    y = y.to_dict('records')[0]
+    values = [x,y]
+    for i in values:
+    for k,v in i:
+        xprob = 0
+        yprob = 0
+        for i in data['class'].unique():
+            xprob += getProb(data, k, v, i)
+            yprob += getProb(data, k, y.get)
+            dist +=
+
+
+    pass
+
+def getProb(data,V_i, v_i, c_a): # gets P(Ca | Vi)
+    x = data.loc[data[V_i] == v_i]  # Finds rows with the given feature value
+    y = x.loc[data['class'] == c_a]  # Finds rows matching class that have the given feature and value
+    z = data[data['class'] == c_a]  # Finds only rows containing the given class
+    c_aCount = len(z)
+    c_aANDv_i = len(y)
+    prob = c_aANDv_i / c_aCount
+    return prob
+
+
 def EuclideanD(x,y):
     distance = 0
     for i in range(x.shape[1]-1):
@@ -81,7 +110,11 @@ def kindaNN():
         x = samples.iloc[[0]]
         y = samples.drop(x.index)
 
+        VDM2(train,x,y)
+
         print(x)
         print(y)
 
         print("Distance:",VDM(train,x,y))
+
+kindaNN()
